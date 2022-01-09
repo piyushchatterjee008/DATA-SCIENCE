2389986a293b1fb17ebe1575745362daca4e2efa
piyushchatterjee008 <36198356+piyushchatterjee008@users.noreply.github.com>
2022-01-09 09:57:27 +0530

    Python programs

---
+++ module.py
@@ -0,0 +1,12 @@
+a = 'Happy'
+b='happy b'
+def print_name(name):
+	print("Hello ", name, "!!!")
+    
+def test():
+    print("test")
+    
+my_list = [1,2,3,4,5]
+for i in my_list:
+    print(i)
+    
